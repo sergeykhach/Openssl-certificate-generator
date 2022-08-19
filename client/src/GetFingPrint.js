88d48ac1aaffda3e6f037f3c2283f607a837fd75
sergeykhach <sosesallc@gmail.com>
2022-08-19 13:37:38 +0400

    11. build araca qrqoderov svg u png+saverov

--- client/src/GetFingPrint.js
+++ client/src/GetFingPrint.js
@@ -1,8 +1,6 @@
-
-//import $ from "jquery";
-import Web3 from 'https://cdn.esm.sh/v58/web3@1.6.1/es2021/web3.js'
+import Web3 from 'web3';
 const web3 = new Web3(window.web3.currentProvider);
-const { ethereum } = window;
+
 let contractAddrOwn = "0x2d28Dafd034fAB7eF324Bbb659D669263b326373";
 let abiOwn =  [
 	{
